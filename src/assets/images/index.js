806f5a0b34f037300e1166edabfe19607e185dcd
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2025-03-08 10:44:51 +0100

    updated projects and resume

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,5 @@
 import Brightflare from "./brightflare.svg";
-import ValeValeting from "./vale-valeting.png";
+import ValeValeting from "./vale-valeting.svg";
 import CW24 from "./cw24.svg";
 
 export { Brightflare, ValeValeting, CW24 };
